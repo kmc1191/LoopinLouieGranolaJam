9ac965c423a787a924bf6cb9d951e7a9bd2c735a
Adrian Burgos <awb8593@rit.edu>
2024-10-05 16:41:52 -0400

    Fix for locking in X position of catapult

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -3,9 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+var X_POS;
 
+func _ready():
+	X_POS = position.x
 
-func _physics_process(delta: float) -> void:
+func _process(delta):
+	if position.x != X_POS:
+		position.x = X_POS
+	
+
+func _physics_process(delta: float) -> void:	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
