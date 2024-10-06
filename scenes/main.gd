ec164180a163d24c4a3b9edfdce606206aaf179c
Kaitlynn Clement <kmc1191@rit.edu>
2024-10-05 20:24:02 -0400

    Change name of borders + funcs

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,28 +1,26 @@
 extends Sprite2D
 
-var score := [0, 0]# 0:Player, 1: CPU
+var score := [3, 3]# 0:Player, 1: Player 2
 const PADDLE_SPEED : int = 500
 
 #func _on_ball_timer_timeout():
 	#$Ball.new_ball()
 
-func _on_score_left_body_entered(body):
+func _on_wall_left_hit(body):
 	#score[1] += 1
 	#$Hud/CPUScore.text = str(score[1])
 	#$BallTimer.start()
 	$Louie.turn_around()
 	$Louie.jump()
 
-func _on_score_right_body_entered(body):
-	#score[0] += 1
-	#$Hud/PlayerScore.text = str(score[0])
-	#$BallTimer.start()
-	$Louie.turn_around()
-	$Louie.jump()
-
-
 func _on_hay_bail_collider_body_entered(body: Node2D) -> void:
 	print(body.name)
 	if body.name == "Louie":
 		$Louie.jump()
 		print("hay bail")
+
+
+
+func _on_wall_right_hit(body: Node2D) -> void:
+	$Louie.turn_around()
+	$Louie.jump()
