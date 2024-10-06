de8423b5cd110b2a334d48bcb545a78359b412cb
Adrian Burgos <awb8593@rit.edu>
2024-10-06 06:44:48 -0400

    Hover buttons

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -7,6 +7,19 @@ var playerWon = 0
 @onready var endGameScreen = $endScreen
 
 @onready var mainScreen = $Main
+@onready var playButton = $beforeScreen/playButton/Sprite2D
+@onready var exitButton = $beforeScreen/quitGameButton/Sprite2D
+@onready var homeButton = $endScreen/Sprite2D
+@onready var replayButton = $endScreen/RestartButton/Sprite2D
+
+var playSprite = preload("res://assets/PlayButton.png")
+var playHoverSprite = preload("res://assets/PlayButtonHover.png")
+var homeButtonSprite = preload("res://assets/HomeButton.png")
+var homeButtonHoverSprite = preload("res://assets/HomeButtonHover.png")
+var exitSprite = preload("res://assets/Exit.png")
+var exitHoverSprite = preload("res://assets/ExitHover.png")
+var replayButtonSprite = preload("res://assets/Replay.png")
+var replayButtonHoverSprite = preload("res://assets/ReplayHover.png")
 
 func _ready():
 	pass
@@ -33,3 +46,35 @@ func _on_play_button_pressed() -> void:
 func _on_restart_button_pressed() -> void:
 	game_restarted.emit()
 	$endScreen.visible = false
+
+
+func _on_play_button_mouse_entered() -> void:
+	playButton.texture = playHoverSprite
+
+
+func _on_play_button_mouse_exited() -> void:
+	playButton.texture = playSprite
+
+
+func _on_quit_game_button_mouse_entered() -> void:
+	exitButton.texture = exitHoverSprite
+
+
+func _on_quit_game_button_mouse_exited() -> void:
+	exitButton.texture = exitSprite
+
+
+func _on_home_screen_button_mouse_entered() -> void:
+	homeButton.texture = homeButtonHoverSprite
+
+
+func _on_home_screen_button_mouse_exited() -> void:
+	homeButton.texture = homeButtonSprite
+
+
+func _on_restart_button_mouse_entered() -> void:
+	replayButton.texture = replayButtonHoverSprite
+
+
+func _on_restart_button_mouse_exited() -> void:
+	replayButton.texture = replayButtonSprite
