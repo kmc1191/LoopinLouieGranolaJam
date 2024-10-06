7a9e665821b192a04e57b56d1be9e427d0135969
Kaitlynn Clement <kmc1191@rit.edu>
2024-10-05 22:02:17 -0400

    Add overall scene view

--- scenes/main.gd
+++ scenes/main.gd
@@ -12,29 +12,24 @@ func _on_wall_left_hit(body):
 	#score[1] += 1
 	#$Hud/CPUScore.text = str(score[1])
 	#$BallTimer.start()
-	$Louie.turn_around()
-	$Louie.jump()
-	_end_game()
+	$mainGameScreen/Louie.turn_around()
+	$mainGameScreen/Louie.jump()
 
 func _on_hay_bail_collider_body_entered(body: Node2D) -> void:
 	if body.name == "Louie":
-		$Louie.jump()
-
-
+		$mainGameScreen/Louie.jump()
 
 func _on_wall_right_hit(body: Node2D) -> void:
-	$Louie.turn_around()
-	$Louie.jump()	
-
+	$mainGameScreen/Louie.turn_around()
+	$mainGameScreen/Louie.jump()	
 
 func _on_coop_1_area_body_entered(body: Node2D) -> void:
 	score[1] += 1
-	$Louie.jump()
-
+	$mainGameScreen/Louie.jump()
 
 func _on_coop_2_area_body_entered(body: Node2D) -> void:
 	score[0] += 1
-	$Louie.jump()
+	$mainGameScreen/Louie.jump()
 	
 func _end_game():
 	var playerWon = 0
