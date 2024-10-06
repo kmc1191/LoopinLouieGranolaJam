5a40f64b454a561a3f164003c6e99c926f950a4f
Adrian Burgos <awb8593@rit.edu>
2024-10-05 23:24:36 -0400

    Haybail and Coop asset updates

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+@onready var haybail_sprite = $Sprite2D
+
+var idle_texture = preload("res://assets/HaybaleStationary.png")
+var up_texture = preload("res://assets/HaybaleUp.png")
+var down_texture = preload("res://assets/HaybaleDown.png")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +18,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	# Change animations
+	if velocity.y < 0:
+		haybail_sprite.texture = up_texture
+	elif velocity.y > 0:
+		haybail_sprite.texture = down_texture
+	else:
+		if is_on_floor():
+			haybail_sprite.texture = idle_texture
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
