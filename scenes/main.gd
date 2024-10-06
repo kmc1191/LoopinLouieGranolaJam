086ea3589a0fc21ec2e21bdd1c5691ac2fde4c6b
Kaitlynn Clement <kmc1191@rit.edu>
2024-10-05 22:31:24 -0400

    Scores fixed

--- scenes/main.gd
+++ scenes/main.gd
@@ -24,19 +24,24 @@ func _on_wall_right_hit(body: Node2D) -> void:
 	$mainGameScreen/Louie.jump()	
 
 func _on_coop_1_area_body_entered(body: Node2D) -> void:
-	score[1] += 1
+	score[0] -= 1
+	print("hit coop 1")
+	$mainGameScreen/Hud/PlayerScore.text = str(score[0])
+	if(score[0] == 0):
+		_end_game(2)
 	$mainGameScreen/Louie.jump()
 
 func _on_coop_2_area_body_entered(body: Node2D) -> void:
-	score[0] += 1
+	print("hit coop 2")
+	score[1] -= 1
+	$mainGameScreen/Hud/CPUScore.text = str(score[1])
+	if(score[1] == 0):
+		_end_game(1)
 	$mainGameScreen/Louie.jump()
 	
-func _end_game():
-	var playerWon = 0
-	if(score[0] == 0):
-		playerWon = 2
-	else:
-		playerWon = 1
-	$UI.on_game_over(playerWon)
+func _end_game(player_won : int):
+	$mainGameScreen.visible = false
+	$mainGameScreen/Hud.visible = false
+	$UI.on_game_over(player_won)
 	
 	
